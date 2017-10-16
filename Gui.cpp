3f3a4a390145035687fc4e3958d710160d0a8dde
justin-segler <justins@cs.utexas.edu>
2017-10-16 16:09:18 -0500

    GUI tweaks

--- Gui.cpp
+++ Gui.cpp
@@ -20,18 +20,18 @@ void Gui::createWindow(void){
 	CEGUI::Window *sheet = wmgr.createWindow("DefaultWindow", "root");
 
 	CEGUI::Window *title = wmgr.createWindow("TaharezLook/Titlebar", "title");
-	title->setText("Racket Game");
-	title->setSize(CEGUI::USize(CEGUI::UDim(0.17, 0), CEGUI::UDim(0.1, 0)));
-	title->setPosition(CEGUI::UVector2(CEGUI::UDim(0.45, 0), CEGUI::UDim(0, 0)));
+	title->setText("  The Racket Game");
+	title->setSize(CEGUI::USize(CEGUI::UDim(0.2, 0), CEGUI::UDim(0.1, 0)));
+	title->setPosition(CEGUI::UVector2(CEGUI::UDim(0.4, 0), CEGUI::UDim(0, 0)));
 	sheet->addChild(title);
 
 	wScore = wmgr.createWindow( "TaharezLook/StaticText", "score" );
-	wScore->setText("Score: " +  std::to_string(score));
+	wScore->setText("   Score: " +  std::to_string(score));
 	wScore->setSize(CEGUI::USize(CEGUI::UDim(0.1, 0), CEGUI::UDim(0.1, 0)));
 	sheet->addChild(wScore);
 
 	wResetText = wmgr.createWindow( "TaharezLook/StaticText", "resetText" );
-	wResetText->setText("To reset the ball, press spacebar.");
+	wResetText->setText("    To reset the ball, press spacebar.");
 	wResetText->setSize(CEGUI::USize(CEGUI::UDim(0.3, 0), CEGUI::UDim(0.1, 0)));
 	wResetText->setPosition(CEGUI::UVector2(CEGUI::UDim(0.7, 0), CEGUI::UDim(0, 0)));
 	sheet->addChild(wResetText);
@@ -43,22 +43,22 @@ void Gui::createWindow(void){
 }
 int Gui::increaseScore(void){
 	score++;
-	wScore->setText("Score: " + std::to_string(score));
+	wScore->setText("   Score: " + std::to_string(score));
 	return score;
 
 }
 int Gui::increaseScore(int num){
 	score+=num;
-	wScore->setText("Score: " + std::to_string(score));
+	wScore->setText("   Score: " + std::to_string(score));
 	return score;
 
 }
 int Gui::decreaseScore(void){
 	score--;
-	wScore->setText("Score: " + std::to_string(score));
+	wScore->setText("   Score: " + std::to_string(score));
 	return score;
 }
 void Gui::resetScore(){
 	score = 0;
-	wScore->setText("Score: " + std::to_string(score));
+	wScore->setText("   Score: " + std::to_string(score));
 }
